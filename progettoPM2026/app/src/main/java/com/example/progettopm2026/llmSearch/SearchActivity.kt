c2e0cc95007d96743734d4c637d4f2d30ba6a2ba
bloodeagle092 <121015651+bloodeagle092@users.noreply.github.com>
2026-04-26 10:58:15 +0200

    minor fix activity search

--- progettoPM2026/app/src/main/java/com/example/progettopm2026/llmSearch/SearchActivity.kt
+++ progettoPM2026/app/src/main/java/com/example/progettopm2026/llmSearch/SearchActivity.kt
@@ -6,16 +6,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.progettopm2026.R
-
+import com.example.progettopm2026.databinding.ActivitySearchBinding
 class SearchActivity: AppCompatActivity(){
+    private lateinit var binding: ActivitySearchBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupClickListeners()
+    }
+    private fun setupClickListeners() {
+        binding.buttonBack.setOnClickListener { finish() }
     }
 }
